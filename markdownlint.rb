e7856050102b4403c3db7cabe0d4494cb281b02f
Brian Sage <github@sagehome.com>
2019-04-01 21:28:09 -0600

    Add linter

---
+++ markdownlint.rb
@@ -0,0 +1,7 @@
+# MarkdownLint Style file
+# See https://github.com/markdownlint/markdownlint/blob/master/docs/configuration.md
+
+all
+exclude_rule 'MD013'
+exclude_rule 'MD033'
+exclude_rule 'MD041'
